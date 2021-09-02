5a2c8019cd25dd6bc811d828e8c5abccfc589aab
shuvangkar <sshuvo93@gmail.com>
2021-09-01 23:27:00 -0400

    comment added with the script

--- Change Simulink parameters using Matlab script/update_simulink_params.m
+++ Change Simulink parameters using Matlab script/update_simulink_params.m
@@ -1,3 +1,5 @@
+
+% Author:  Shuvangkar Das
 clc;
 close all;
 clear all;
@@ -7,11 +9,11 @@ clear all;
 % You can also see the model in text file using the command 
 % edit('sim_model.mdl') . edit() function works on .mdl file
 
-get_param('sim_model/Gain1','Gain');
-set_param('sim_model/Gain1','Gain','50')
+get_param('sim_model/Gain1','Gain');     % Get the parameter current value
+set_param('sim_model/Gain1','Gain','50') % Setting a new value for the gain parameter
 
 for gainVal = 0:50
-    gainVal
+    gainVal  % Printing the value
     set_param('sim_model/Gain1','Gain',int2str(gainVal))
     pause(2);
 end
